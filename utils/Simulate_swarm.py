e91406967db044a4381013a8d2b19984c2dc91dd
tugayalperen <tugay.karaguzel@gmail.com>
2021-11-15 17:37:42 +0100

    Fitness calculator class and sensor class is added

--- utils/Simulate_swarm.py
+++ utils/Simulate_swarm.py
@@ -1,23 +1,18 @@
 """
 Loading and testing
 """
-import time
-from typing import AnyStr
-
 from isaacgym import gymapi
 from isaacgym import gymutil
-from uuid import uuid4
-import math
+from scipy.spatial.transform import Rotation as R
+
 import numpy as np
-from sklearn.neighbors import NearestNeighbors
+
+from calculate_fitness import FitnessCalculator  # Fitness calculator class, all functions are implemented as different
+# methods of this class
+from sensors import Sensors  # Sensor class, all types of sensors are are implemented as different
+# methods of this class
 
 from . import Controllers
-import os
-
-def get_nearest_neighbours_states(swarm_state: np.array, ind_state: np.array):
-    neigh = NearestNeighbors(n_neighbors=2, radius=10)
-    neigh.fit(swarm_state)
-    neigh.kneighbors(ind_state)
 
 def simulate_swarm(life_timeout: float, individual: Controllers.Controller, headless: bool) -> np.array:
     """
@@ -103,6 +98,7 @@ def simulate_swarm(life_timeout: float, individual: Controllers.Controller, head
     num_robots = 14
     distance = 0.5
     robot_handles = []
+    initial_positions = np.zeros((2, num_robots))  # Allocation to save initial positions of robots
 
     controller_update_time = 0.2
     rows = 4
@@ -116,6 +112,8 @@ def simulate_swarm(life_timeout: float, individual: Controllers.Controller, head
             (((i + 9) // 12 + i) % rows) * distance - (rows - 1) / 2 * distance,
             (((i + 9) // 12 + i) // rows) * distance - (rows - 1) / 2 * distance,
             0.033)
+        initial_positions[0][i] = pose.p.x  # Save initial position x of i'th robot
+        initial_positions[1][i] = pose.p.y  # Save initial position y of i'th robot
 
         print("Loading asset '%s' from '%s', #%i" % (robot_asset_file, asset_root, i))
         robot_asset = gym.load_asset(
@@ -149,27 +147,65 @@ def simulate_swarm(life_timeout: float, individual: Controllers.Controller, head
     # # subscribe to spacebar event for reset
     gym.subscribe_viewer_keyboard_event(viewer, gymapi.KEY_R, "reset")
 
-    def update_robot():
-        # gym.clear_lines(viewer)
-        swarm_state = np.copy(gym.get_sim_rigid_body_states(sim, gymapi.STATE_ALL))[::4]
+    def update_robot(sensor_input_distance, sensor_input_heading):
         for i in range(num_robots):
-            robot_handle = robot_handles[i]
+            # state : np.array() --> 5 by 1. [0:3] --> distance sensor output, [4] --> heading sensor output
+            state = np.array([sensor_input_distance[i, :], sensor_input_heading[i]])
+            velocity_target = individual.velocity_commands(state)  # assumed to be in format of [u,w]
+            n_l = (velocity_target[0] - (velocity_target[1] / 2) * 0.085) / 0.021
+            n_r = (velocity_target[0] + (velocity_target[1] / 2) * 0.085) / 0.021
+            gym.set_actor_dof_velocity_targets(env, robot_handles[i], [n_l, n_r])
 
-            states = gym.get_actor_rigid_body_states(env, robot_handle, gymapi.STATE_ALL)["pose"]["p"][0]
+    def get_pos_and_headings():
+        headings = []
+        positions_x = []
+        positions_y = []
 
-            velocity_target = individual.velocity_commands(states) * velocity_limits
-            gym.set_actor_dof_velocity_targets(env, robot_handle, velocity_target)
+        for i in range(num_robots):
+            body_angle = gym.get_actor_rigid_body_states(env, robot_handles[i], gymapi.STATE_POS)["pose"]["r"][0]
+            body_angle_mat = np.array([body_angle[0], body_angle[1], body_angle[2], body_angle[3]])
+            r = R.from_quat(body_angle_mat)
+            r = r.as_euler('zyx')
+            headings.append(r[0])
+
+            position = gym.get_actor_rigid_body_states(env, robot_handles[i], gymapi.STATE_POS)["pose"]["p"][0:1]
+            positions_x.append(position[0][0])
+            positions_y.append(position[0][1])
+
+            return (np.array(headings), np.array(positions_x), np.array(positions_y))
 
     t = 0
-    swarm_states = []
+
+    desired_movement = 10  # This values is required for the "movement" metric
+    timestep = 0  # Counter to save total time steps, required for final step of fitness value calculation
+
+    positions = np.full([3, num_robots], 0.01)  # Allocation to save positions of all robots in a single matrix
+    fitness_calculator = FitnessCalculator(num_robots, initial_positions, desired_movement)  #  Fitness calculator init
+    sensor = Sensors()  # Sensors init
+
     # %% Simulate
     while t <= life_timeout:
         # Every 0.01 seconds the velocity of the joints is set
         t = gym.get_sim_time(sim)
 
         if round(t, 3) % controller_update_time == 0.0:
-            swarm_states.append(np.copy(gym.get_sim_rigid_body_states(sim, gymapi.STATE_ALL))[::4])
-            update_robot()
+            headings, positions[0], positions[1] = get_pos_and_headings()  # Update positions and headings of all robots
+
+            distance_sensor_outputs = sensor.four_dir_sensor(positions, headings)  # The values recorded by on-board
+            # sensors of robots
+            heading_sensor_outputs = sensor.heading_sensor(positions, headings)  # The values recorded by on-board
+            # sensors of robots
+            update_robot(distance_sensor_outputs, heading_sensor_outputs)  # The connection to robot controller.
+            # Ideally, this function accepts sensor readings, evaluates them on a controller (NN) and applies actuation
+            # commands for each robot.
+
+            timestep = timestep + 1  # Time step counter
+            fitness_coh_and_sep = fitness_calculator.calculate_cohesion_and_separation(positions)  # Update fitness val
+            fitness_alignment = fitness_calculator.calculate_alignment(headings)  # Update fitness val
+            fitness_movement = fitness_calculator.calculate_movement(positions)  # Update fitness val
+
+            # nogs = fitness_calculator.calculate_number_of_groups(positions)  # Update fitness val
+
         # Step the physics
         gym.simulate(sim)
         gym.fetch_results(sim, True)
@@ -178,4 +214,7 @@ def simulate_swarm(life_timeout: float, individual: Controllers.Controller, head
 
     gym.destroy_viewer(viewer)
     gym.destroy_sim(sim)
-    return swarm_states
+
+    experiment_fitness = fitness_coh_and_sep[0] / timestep - fitness_coh_and_sep[
+        1] / timestep + fitness_alignment / timestep + fitness_movement  # For the time average, divide by time step
+    return experiment_fitness
---
+++ utils/calculate_fitness.py
@@ -0,0 +1,214 @@
+import numpy as np
+from copy import deepcopy
+
+class FitnessCalculator:
+
+    def __init__(self, num_robots, initial_positions, desired_movement):
+        # A calculator class for all fitness functions/elements.
+
+        # Arguments:
+        # num_robots (integer) : Number of robots in the swarm
+        # initial_positions : np.array(2,number_of_robots) --> initial x positions of all robots,  positions[1][:] -->
+        # initial y positions of all robots
+        # desired_movement (float) : The desired distance to be traveled by the swarm. Defined in "Evolving flocking in
+        # embodied agents based on local and global application of Reynolds"
+
+        self.current_cohesion = 0
+        self.current_separation = 0
+        self.current_alignment = 0
+        self.current_movement = 0
+
+        self.num_robots = num_robots
+        self.initial_positions = initial_positions
+        self.cohesion_range = 2.0  # The range to accept as a "neighborhood" of the focal robot
+        self.desired_movement = desired_movement
+
+        self.dij = np.zeros((self.num_robots,self.num_robots))
+
+    def calculate_cohesion_and_separation(self,positions):
+        # The "cohesion" and "separation", fitness function elements, as in "Evolving flocking in embodied agents based
+        # on local and global application of Reynolds".
+
+        # Input:
+        # positions : np.array(2,number_of_robots), positions[0][:] --> x positions of all robots, positions[1][:] --> y
+        # positions of all robots
+
+        # Output:
+        # np.array([current_cohesion], [current_separation]) --> In each call of this function, the "cohesion" and
+        # "separation" values are returned. In order to get a value between 0-1 for each (time average), the value
+        # returned by this function should be divided by the number which this function is called (time step).
+
+        # In case of no neighbors, "cohesion" is calculated as zero for that robot, separation is naturally zero.
+
+        distance_to_com_of_neighbors = np.zeros((1, self.num_robots))
+        cohesion_metric = np.zeros((1, self.num_robots))
+        collision_counter = np.zeros((1, self.num_robots))
+        collision_assumption = 0.2
+
+        xx1, xx2 = np.meshgrid(positions[0], positions[0])
+        yy1, yy2 = np.meshgrid(positions[1], positions[1])
+        d_ij_x = xx1 - xx2
+        d_ij_y = yy1 - yy2
+        d_ij = np.sqrt(np.multiply(d_ij_x, d_ij_x) + np.multiply(d_ij_y, d_ij_y))
+        self.dij = d_ij  # This is a shared variable
+        neighbors = deepcopy(d_ij)
+        collisions = deepcopy(d_ij)
+
+        collisions[collisions > collision_assumption] = 0.0
+        collision_counter = np.count_nonzero(collisions, axis=1)
+        collision_at_t = np.sum(collision_counter) / self.num_robots
+        self.current_separation = self.current_separation + collision_at_t
+
+        neighbors[neighbors > self.cohesion_range] = 0.0
+        neighbors[neighbors != 0] = 1
+
+        cohesion_at_t = 0
+
+        for i in range(self.num_robots):
+            nsx = np.multiply(d_ij_x, neighbors[i][:])
+            nsx = nsx[np.nonzero(nsx)]
+            nsy = np.multiply(d_ij_y, neighbors[i][:])
+            nsy = nsy[np.nonzero(nsy)]
+
+            if nsx:
+                distance_to_com_of_neighbors_x = np.mean(nsx)
+                distance_to_com_of_neighbors_y = np.mean(nsy)
+
+                distance_to_com_of_neighbors[0][i] = np.sqrt(
+                    np.power(distance_to_com_of_neighbors_x, 2) + np.power(distance_to_com_of_neighbors_y, 2))
+                cohesion_at_t = cohesion_at_t + (1 - (distance_to_com_of_neighbors[0][i] / self.cohesion_range))
+
+        self.current_cohesion = self.current_cohesion + cohesion_at_t / self.num_robots
+
+        return np.array([self.current_cohesion, self.current_separation])
+
+    def calculate_alignment(self, headings):
+        # The "alignment", fitness function element, as in "Evolving flocking in embodied agents based
+        # on local and global application of Reynolds".
+
+        # Requires calculate_cohesion_and_separation() method to work alongside, this method use some information from
+        # there.
+
+        # Input:
+        # headings : np.array(1, number_of_robots), heading angles of all robots, in radians
+
+        # Output:
+        # np.array([current_alignment] --> In each call of this function, the "alignment"
+        # value is returned. In order to get a value between 0-1 for "alignment" (time average), the value
+        # returned by this function should be divided by the number which this function is called (time step).
+
+        # In case of no neighbors, "alignment" is calculated as zero for that robot, separation is naturally zero.
+
+        sum_cosh = np.zeros((1, self.num_robots))
+        sum_sinh = np.zeros((1, self.num_robots))
+        number_of_neighbors = np.zeros((1, self.num_robots))
+        alignment_at_t = 0
+
+        for i in range(0,self.num_robots):
+            sum_cosh[0,i] = np.cos(headings[i])
+            sum_sinh[0,i] = np.sin(headings[i])
+
+            for j in range(0,self.num_robots):
+                if self.dij[i,j] < self.cohesion_range and i != j:
+
+                    sum_cosh[0,i] = sum_cosh[0,i] + np.cos(headings[j])
+                    sum_sinh[0,i] = sum_sinh[0,i] + np.sin(headings[j])
+                    number_of_neighbors[0][i] = number_of_neighbors[0][i] + 1
+
+            if number_of_neighbors[0][i]>0:
+                alignment_at_t = alignment_at_t + np.sqrt(np.power(sum_cosh[0,i],2)+np.power(sum_sinh[0,i],2))/(number_of_neighbors[0][i]+1)
+        self.current_alignment = self.current_alignment + alignment_at_t/self.num_robots
+
+        return np.array([self.current_alignment])
+
+    def calculate_movement(self,positions):
+        # The "movement", fitness function element, as in "Evolving flocking in embodied agents based
+        # on local and global application of Reynolds".
+
+        # Input:
+        # positions : np.array(2,number_of_robots), positions[0][:] --> x positions of all robots, positions[1][:] --> y
+        # positions of all robots
+
+        # Output:
+        # np.array([total_movement] --> In each call of this function, the "movement"
+        # value is returned. In order to get a value between 0-1 for "movement" (time average), the value
+        # returned by this function should be divided by the number which this function is called (time step).
+
+        total_movement = 0
+
+        for i in range(0, self.num_robots):
+            movement_percentage = np.sqrt(np.square(self.initial_positions[0][i]-positions[0][i])+np.square(self.initial_positions[1][i]-positions[1][i]))/self.desired_movement
+
+            if movement_percentage>=1.0:
+                movement_percentage = 1.0
+
+            total_movement = total_movement + movement_percentage
+
+        total_movement = total_movement/self.num_robots
+
+        return np.array([total_movement])
+
+    def calculate_number_of_groups(self,positions):
+        # The number of groups, calculated based on "equivalence classes".
+
+        # Input:
+        # positions : np.array(2,number_of_robots), positions[0][:] --> x positions of all robots, positions[1][:] --> y
+        # positions of all robots
+
+        # Output:
+        # number_of_groups (integer)
+
+        sensing_range = 2.0  # The range to assume robots can sense each other.
+        member_number = np.shape(positions[0])[0]
+        xx1, xx2 = np.meshgrid(positions[0] , positions[0])
+        yy1, yy2 = np.meshgrid(positions[1] , positions[1])
+        d_ij_x = xx1 - xx2
+        d_ij_y = yy1 - yy2
+        d_ij = np.sqrt(np.multiply(d_ij_x, d_ij_x) + np.multiply(d_ij_y,d_ij_y))
+        d_ij = np.triu(d_ij)
+        mm=0
+
+        list1 = []
+        list2 = []
+
+        for j in range(member_number):
+            for k in range(member_number):
+                if (d_ij[j,k]<sensing_range) and (j!=k) and (d_ij[j,k]!=0):
+                    mm = mm + 1
+                    list1.append(j)
+                    list2.append(k)
+
+        list1 = np.array(list1)
+        list2 = np.array(list2)
+
+        m = mm
+        n = member_number
+        nf = []
+
+        for k in range(n):
+            nf.append(k)
+
+        nf = np.array(nf)
+
+        for l in range(m):
+            j = list1[l]
+
+            while nf[j] != j:
+                j = nf[j]
+
+            k = list2[l]
+
+            while nf[k] !=k:
+                k = nf[k]
+
+            if j != k:
+                nf[j] = k
+
+        for j in range(n):
+            while nf[j] != nf[nf[j]]:
+                nf[j] = nf[nf[j]]
+
+        uniques = np.unique(nf)
+        number_of_groups = uniques.shape[0]
+
+        return number_of_groups
