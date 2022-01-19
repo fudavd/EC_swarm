heading average is now single output out of threshold 4dir sensor readings are now 2.0 error in grad fitness fixed

utils/Simulate_swarm.py
@@ -224,7 +224,7 @@ def simulate_swarm(life_timeout: float, individual: Individual, headless: bool, 
             elif controller_type == "2dir":
                 state = np.hstack((sensor_input_distance[ii, :], sensor_input_heading[ii, :], own_headings[ii]))
             elif controller_type == "NN":
-                state = np.hstack((sensor_input_distance[ii, :], sensor_input_heading[ii, :], own_headings[ii],
+                state = np.hstack((sensor_input_distance[ii, :], sensor_input_heading[ii], own_headings[ii],
                                    sensor_input_grad[ii]))
             elif controller_type == "default":
                 state = np.empty(controller.n_input)

utils/sensors.py
@@ -312,7 +312,8 @@ class Sensors:
 
             hbars[i, 0] = np.divide(sum_cosh[0, i], np.sqrt(np.square(sum_cosh[0, i]) + np.square(sum_sinh[0, i])))
             hbars[i, 1] = np.divide(sum_sinh[0, i], np.sqrt(np.square(sum_cosh[0, i]) + np.square(sum_sinh[0, i])))
-            average_angle = np.arctan2(hbars[i, 1], hbars[i, 0])
+
+        average_angle = np.arctan2(hbars[:, 1], hbars[:, 0])
 
         return average_angle
 