18ae1ea076ff0ae1e0b5ed8342c28341965f3539
Fuda van Diggelen <fuda.van.diggelen@vu.nl>
2023-03-09 13:19:05 +0100

    Bounded CMAES
    might not be smarter than before

--- utils/CMAES.py
+++ utils/CMAES.py
@@ -18,7 +18,11 @@ class CMAes(EA.EA):
         self.f_new = np.empty(self.Np)
 
         opts = {
-            'popsize': params['pop_size']
+            'popsize': params['pop_size'],
+            'bounds': (
+                [self.bounds[0]] * self.D, # lower bounds per dimension
+                [self.bounds[1]] * self.D, # upper bounds per dimension
+	        ),
         }
         self.es = cma.CMAEvolutionStrategy(solution_seed, params['sigma0'], inopts=opts)
         self.x_new = np.array(self.es.ask())
@@ -30,6 +34,7 @@ class CMAes(EA.EA):
         self.x_best_so_far = []
 
     def get_new_genome(self):
+        ## CMA Documentation Example:
         # while not self.es.stop():
         #     solutions = self.es.ask()
         #     fitness = [cma.ff.rosen(x) for x in solutions]
@@ -37,6 +42,7 @@ class CMAes(EA.EA):
         #     self.es.logger.add() # write data to disc to be plotted
         #     self.es.disp()
         # self.es.result.pretty()
+
         pop_best = np.min(self.f_new)  # some book keeping
         if self.f_best_so_far == [] or pop_best < self.f_best_so_far[-1]:
             self.f_best_so_far.append(pop_best)
@@ -46,9 +52,11 @@ class CMAes(EA.EA):
             self.x_best_so_far.append(self.x_best_so_far[-1])
 
         self.es.tell(self.x_new, self.f_new)
+        # self.es.disp()
         self.x = copy.deepcopy(self.x_new)
         self.f = copy.deepcopy(self.f_new)
         self.x_new = np.array(self.es.ask())
-        self.x_new = np.clip(self.x_new, self.bounds[0], self.bounds[1])
+        # clipped = np.clip(self.x_new, self.bounds[0], self.bounds[1])
+        # assert (self.x_new == clipped).all()
         return self.x_new
 
